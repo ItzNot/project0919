a425442f153ad4224616dbb69c93aa4366cb5097
ItzNot <dkskd8888@gmail.com>
2022-09-20 12:27:05 +0900

    화면 구성만 완성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Carousel from 'react-bootstrap/Carousel';
 
 
+
 function App() {
   return (
     <div className="App">
@@ -10,25 +11,25 @@ function App() {
         <span>로그인</span>|<span>회원가입</span>
       </div>
       <div className='header-bar'>
-        <div style={{width:'200px', height:'75px', backgroundColor:'gray'}}>사진부분</div>
+        <div className='k-logo'></div>
         <div className='team-logo'>
-          <div style={{width:'75px', height:'75px'}}>사진1</div>
-          <div style={{width:'75px', height:'75px'}}>사진2</div>
+          <div className='ulsan'></div>
+          <div className='jeju'></div>
         </div>
       </div>
       <div className="header-menubar">
-        <ul style={{listStyleType: "none", display:'flex', justifyContent: 'space-around', margin:'0 auto'}}>
+        <ul style={{listStyleType: "none", display:'flex',flexDirection:'row' , justifyContent:'space-around', color:'white', padding:'10px 0', margin:"0"}}>
           <li>뉴스</li>
           <li>영상</li>
           <li>일정/결과/티켓</li>
           <li>이벤트</li>
           <li>About K League</li>
+          <li>검색</li>
         </ul>
       </div>
-      <Carousel style={{width:'100%', height:'400px', backgroundColor:'gray'}}>
+      <Carousel style={{width:'100%', height:'400px'}}>
         <Carousel.Item>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <div className='eventBanner1'></div>
         </Carousel.Item>
         <Carousel.Item>
         <h3>Second slide label</h3>
@@ -39,8 +40,23 @@ function App() {
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Item>
       </Carousel>
+      <div className='mid-site'>
+        <div className='aside-left'>
+          <div className='rankTextbar'>
+            <div className='ballImage'/>
+            <p>팀 순위</p>
+          </div>
+          <div className='rankImage'>
+            
+          </div>
+        </div>
+        <div className='aside-right'></div>
+        
+      </div>
+      
     </div>
   );
 }
+
 
 export default App;
